0571335c3ed85244f10a14e32e04c09a85629445
clay austin <cgaustin@usgs.gov>
2016-02-04 09:27:49 -0600

    add smtphandler to api logging

--- api/api_logging.py
+++ api/api_logging.py
@@ -3,9 +3,13 @@ import logging
 
 from logging import FileHandler
 from logging import Formatter
+from logging.handlers import SMTPHandler
+from api.utils import api_cfg
 
 if not os.path.exists("logs"):
     os.mkdir("logs")
+
+cfg = api_cfg()
 
 LOG_FORMAT = ("%(asctime)s [%(levelname)s]: %(message)s in %(pathname)s:%(lineno)d")
 
@@ -14,13 +18,15 @@ api_logger.setLevel(logging.DEBUG)
 
 ih = FileHandler("logs/api-info.log")
 dh = FileHandler("logs/api-debug.log")
+eh = SMTPHandler(mailhost='localhost', fromaddr=cfg['apiemailsender'], toaddrs=cfg['apiemailreceive'].split(','), subject='API ERROR')
 
 ih.setLevel(logging.WARN)
 dh.setLevel(logging.DEBUG)
+eh.setLevel(logging.DEBUG)
 
 ih.setFormatter(Formatter(LOG_FORMAT))
 dh.setFormatter(Formatter(LOG_FORMAT))
 
 api_logger.addHandler(ih)
 api_logger.addHandler(dh)
-
+api_logger.addHandler(eh)
