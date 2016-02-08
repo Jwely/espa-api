5ef88960d2276ce1c8e95176752a32629b0b9366
klsmith-usgs <klsmith@usgs.gov>
2016-02-08 10:58:21 -0600

    continued work setting up validation tests

--- test/version0_testorders.py
+++ test/version0_testorders.py
@@ -1,62 +1,83 @@
+import copy
+
 import api.domain.sensor as sn
 
-"""
-base = {'MOD09A1': {'inputs': 'MOD09A1.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MOD09GA': {'inputs': 'MOD09GA.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MOD09GQ': {'inputs': 'MOD09GQ.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MOD09Q1': {'inputs': 'MOD09Q1.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MYD09A1': {'inputs': 'MYD09A1.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MYD09GA': {'inputs': 'MYD09GA.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MYD09GQ': {'inputs': 'MYD09GQ.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MYD09Q1': {'inputs': 'MYD09Q1.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MOD13A1': {'inputs': 'MOD13A1.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MOD13A2': {'inputs': 'MOD13A2.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MOD13A3': {'inputs': 'MOD13A3.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MOD13Q1': {'inputs': 'MOD13Q1.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MYD13A1': {'inputs': 'MYD13A1.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MYD13A2': {'inputs': 'MYD13A2.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MYD13A3': {'inputs': 'MYD13A3.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'MYD13Q1': {'inputs': 'MYD13Q1.A2000072.h02v09.005.2008237032813',
-                    'products': ['l1']},
-        'tm4': {'inputs': 'LT42181092013069PFS00',
-                'products': ['l1']},
-        'tm5': {'inputs': 'LT52181092013069PFS00',
-                'products': ['l1']},
-        'etm7': {'inputs': 'LE72181092013069PFS00',
-                 'products': ['l1']},
-        'oli8': {'inputs': 'LO82181092013069PFS00',
-                 'products': ['l1']},
-        'olitirs8': {'inputs': 'LC82181092013069PFS00',
-                     'products': ['l1']},
-        'projection': {'lonlat': None},
-        'image_extents': {'north': 0.0002695,
-                          'south': 0,
-                          'east': 0.0002695,
-                          'west': 0,
-                          'units': 'dd'},
-        'format': 'gtiff',
-        'resampling_method': 'cc',
-        'resize': {'pixel_size': 0.0002695,
-                   'pixel_size_units': 'dd'},
-        'plot_statistics': True}"""
+
+good_test_projections = {'aea': {'standard_parallel_1': 29.5,
+                                 'standard_parallel_2': 45.5,
+                                 'central_meridian': -96,
+                                 'latitude_of_origin': 23,
+                                 'false_easting': 0,
+                                 'false_northing': 0,
+                                 'datum': 'nad83'},
+                         'utm': {'zone': 33,
+                                 'zone_ns': 'south'},
+                         'lonlat': None,
+                         'sinu': {'central_meridian': 0,
+                                  'false_easting': 0,
+                                  'false_northing': 0},
+                         'ps': {'longitudinal_pole': 0,
+                                'latitude_true_scale': 75}}
 
 
 def build_base_order():
+    """
+    Builds the following dictionary (with the products filled out from sensor.py):
+
+    base = {'MOD09A1': {'inputs': 'MOD09A1.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MOD09GA': {'inputs': 'MOD09GA.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MOD09GQ': {'inputs': 'MOD09GQ.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MOD09Q1': {'inputs': 'MOD09Q1.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MYD09A1': {'inputs': 'MYD09A1.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MYD09GA': {'inputs': 'MYD09GA.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MYD09GQ': {'inputs': 'MYD09GQ.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MYD09Q1': {'inputs': 'MYD09Q1.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MOD13A1': {'inputs': 'MOD13A1.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MOD13A2': {'inputs': 'MOD13A2.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MOD13A3': {'inputs': 'MOD13A3.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MOD13Q1': {'inputs': 'MOD13Q1.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MYD13A1': {'inputs': 'MYD13A1.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MYD13A2': {'inputs': 'MYD13A2.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MYD13A3': {'inputs': 'MYD13A3.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'MYD13Q1': {'inputs': 'MYD13Q1.A2000072.h02v09.005.2008237032813',
+                        'products': ['l1']},
+            'tm4': {'inputs': 'LT42181092013069PFS00',
+                    'products': ['l1']},
+            'tm5': {'inputs': 'LT52181092013069PFS00',
+                    'products': ['l1']},
+            'etm7': {'inputs': 'LE72181092013069PFS00',
+                     'products': ['l1']},
+            'oli8': {'inputs': 'LO82181092013069PFS00',
+                     'products': ['l1']},
+            'olitirs8': {'inputs': 'LC82181092013069PFS00',
+                         'products': ['l1']},
+            'projection': {'lonlat': None},
+            'image_extents': {'north': 0.0002695,
+                              'south': 0,
+                              'east': 0.0002695,
+                              'west': 0,
+                              'units': 'dd'},
+            'format': 'gtiff',
+            'resampling_method': 'cc',
+            'resize': {'pixel_size': 0.0002695,
+                       'pixel_size_units': 'dd'},
+            'plot_statistics': True}"""
+
     base = {'projection': {'lonlat': None},
             'image_extents': {'north': 0.0002695,
                               'south': 0,
@@ -88,22 +109,46 @@ def build_base_order():
     return base
 
 
-good_projs = {'aea': {'standard_parallel_1': 29.5,
-                      'standard_parallel_2': 45.5,
-                      'central_meridian': -96,
-                      'latitude_of_origin': 23,
-                      'false_easting': 0,
-                      'false_northing': 0,
-                      'datum': 'nad83'},
-              'utm': {'zone': 33,
-                      'zone_ns': 'south'},
-              'lonlat': None,
-              'sinu': {'central_meridian': 0,
-                       'false_easting': 0,
-                       'false_northing': 0},
-              'ps': {'longitudinal_pole': 0,
-                     'latitude_true_scale': 75}}
+# Try to make a generator??
+def test_assertion_failures(test_order, schema, path=None):
+    """
+    Use recursion to move through the test_order and schema to build
+    bad orders that will fail validation
+    """
+    if not path:
+        path = tuple()
 
+    base = copy.deepcopy(schema)
+    for key in path:
+        base = base['properties'][key]
 
-if __name__ == '__main__':
-    print build_base_order()
+    for key, val in test_order.items():
+        orig = copy.deepcopy(test_order[key])
+
+        if isinstance(val, dict):
+            # Assert failure substitute type
+            # Assert failure insert bad value
+
+            test_order[key] = copy.deepcopy(orig)
+            next_path = path + (key.lower(),)
+            test_assertion_failures(val, schema, next_path)
+        if isinstance(val, list):
+            constraints = base['properties'][key]
+            # Assert failure substitute type
+            # Assert failure insert bad value
+
+            test_order[key] = copy.deepcopy(orig)
+        if isinstance(val, (float, int, long)):
+            constraints = base['properties'][key]
+            if path and path[-1] == 'image_extents':
+                # Assert extents failures
+                pass
+            # Assert failure substitute type
+            # Assert failure values out of bounds
+
+            test_order[key] = copy.deepcopy(orig)
+        if isinstance(val, (str, unicode)):
+            constraints = base['properties'][key]
+            # Assert failure substitute type
+            # Assert failure substitute bad value
+            test_order[key] = copy.deepcopy(orig)
