7eb056f05252aacedc04926260533caf47bd133a
klsmith-usgs <klsmith@usgs.gov>
2016-03-16 09:49:27 -0500

    nlaps testing

--- api/external/nlaps.py
+++ api/external/nlaps.py
@@ -4,30 +4,31 @@ Author: David V. Hill
 '''
 
 import os
-from api.system.logger import api_logger as logger
+
 
 class NLAPS(object):
+    """
+    Simple class to hold TMA scene id's
+    """
+    path = os.path.join(os.path.dirname(__file__), 'tma_scenes.txt')
 
-    def __init__(self):
-        self.path = os.path.dirname(__file__)
-        self.path = os.path.join(self.path, 'tma_scenes.txt')
-
-        with open(self.path, 'rb') as nl:
-            data = nl.read()
-            self.keys = {k.strip():True for k in data.split('\n')}
-            data = None
+    with open(path, 'r') as f:
+        keys = set(s.strip() for s in f)
 
 
 def products_are_nlaps(product_list):
+    """
+    Compare the requested products with products that cannot be processed
+
+    Changed to use set for performance
+    :param product_list: requested products
+    :return: products that cannot be processed
+    """
 
     if not isinstance(product_list, list):
         raise TypeError("product_list must be an instance of list()")
 
-    results = []
+    nl = NLAPS
+    matches = nl.keys.intersection(set(product_list))
 
-    nl = NLAPS()
-
-    for p in product_list:
-        if p in nl.keys:
-            results.append(p)
-    return results
+    return list(matches)
---
+++ test/test_external.py
@@ -0,0 +1,30 @@
+import unittest
+
+from api.external.nlaps import products_are_nlaps
+
+
+class TestNLAPS(unittest.TestCase):
+    """
+    Provide testing for sorting out NLAPS products
+    """
+    def setUp(self):
+        self.nlaps = ['LT40150231982306AAA02',
+                      'LT40360241982341AAA05',
+                      'LT51392101985039AAA03',
+                      'LT51790261985079AAA04',
+                      'LT50460331985171AAA04']
+
+        self.non_nlaps = ['LT50290302011300PAC01',
+                          'LC80300302016065LGN00',
+                          'LE70300302016057EDC00',
+                          'LE70290302003126EDC00']
+
+    def test_nlaps_prods(self):
+        all = [_ for _ in self.nlaps]
+        all.extend(self.non_nlaps)
+
+        nlaps_prods = products_are_nlaps(all)
+
+        for prod in nlaps_prods:
+            self.assertTrue(prod in self.nlaps)
+            self.assertTrue(prod not in self.non_nlaps)
