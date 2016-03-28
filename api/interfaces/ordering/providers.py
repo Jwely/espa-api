d1d4c1efb2cd74841d12816a9ec5449834e3f91c
klsmith-usgs <klsmith@usgs.gov>
2016-03-28 09:22:26 -0500

    Look at combining validation modules

--- api/interfaces/ordering/providers.py
+++ api/interfaces/ordering/providers.py
@@ -6,7 +6,7 @@ from api.providers.ordering import MockOrderingProvider
 from api.providers.ordering.ordering_provider import OrderingProvider
 from api.providers.production.production_provider import ProductionProvider
 from api.providers.validation import MockValidationProvider
-from api.providers.validation import ValidationProvider
+from api.providers.validation.validictory_validation import ValidationProvider
 from api.providers.configuration.configuration_provider import ConfigurationProvider
 
 
