3790960720aa3841696acb1d41fd2bafe96bacce
klsmith-usgs <klsmith@usgs.gov>
2016-03-31 12:39:47 -0500

    initial work on admin provider

--- api/providers/administration/__init__.py
+++ api/providers/administration/__init__.py
@@ -1,5 +1,70 @@
 import abc
 
+
+class AdministrationProviderException(Exception):
+    pass
+
+
 class AdminProviderInterfaceV0(object):
     __metaclass__ = abc.ABCMeta
 
+    @abc.abstractmethod
+    def configuration(self, key=None, value=None, delete=False):
+        """
+        View or update a configuration key
+        Defaults to listing all configuration keys & values
+
+        :param key: configuration key to look for
+        :param value: new value for the key
+        :param delete: remove key from configuration
+        """
+
+    @abc.abstractmethod
+    def jobs(self, jobid=None, stop=False):
+        """
+        View current hadoop jobs or kill the job
+        Defaults to listing all the jobs
+
+        :param jobid: specific job to look at
+        :param stop: stop the specified job
+        """
+
+    @abc.abstractmethod
+    def system(self, key=None, disposition='on'):
+        """
+        System related operation to switch on/off
+
+        :param disposition: 'on' or 'off'
+        :param key: system or subsystem to toggle
+        """
+
+    @abc.abstractmethod
+    def onlinecache(self, list_orders=False, orderid=None, filename=None,
+                    delete=False):
+        """
+        Operations related to dealing with the LUSTRE cache
+
+        :param delete: delete specified orderid/filename
+        :param list_orders: list current order on the disk
+        :param orderid: list specific order on the disk
+        :param filename: filename for a specified order
+        """
+
+    @abc.abstractmethod
+    def orders(self, query=None, cancel=False):
+        """
+        Provide access to orders
+        Defaults to displaying information for all orders in the system
+
+        :param query: Used to return specific order(s)
+        :param cancel: cancel a specific orderid
+        """
+    @abc.abstractmethod
+    def products(self, query=None, resubmit=None):
+        """
+        Provide access to products in the system
+        Defaults to displaying information for all products in the system
+
+        :param query: list products that meet the specified criteria
+        :param resubmit: resubmits supplied products
+        """
--- api/providers/administration/administration_provider.py
+++ api/providers/administration/administration_provider.py
@@ -1,7 +1,35 @@
+import copy
 
-from api.providers.administration import AdminProviderInterfaceV0
+from api.util.dbconnect import db_instance
+from api.providers.administration import AdminProviderInterfaceV0, AdministrationProviderException
+from api.providers.configuration.configuration_provider import ConfigurationProvider as cp
 
-class AdministrationProviderException(Exception):
-    pass
 
 class AdministrationProvider(AdminProviderInterfaceV0):
+    config = cp()
+    db = db_instance()
+
+    def orders(self, query=None, cancel=False):
+        pass
+
+    def system(self, key=None, disposition='on'):
+        pass
+
+    def products(self, query=None, resubmit=None):
+        pass
+
+    def configuration(self, key=None, value=None, delete=False):
+        if not key:
+            return self.config.configuration_keys
+        elif not delete and not value:
+            return self.config.get(key)
+        elif value and not delete:
+            return self.config.put(key, value)
+        elif value and delete:
+            return self.config.delete(key)
+
+    def onlinecache(self, list_orders=False, orderid=None, filename=None, delete=False):
+        pass
+
+    def jobs(self, jobid=None, stop=False):
+        pass
